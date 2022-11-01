a2ca54f60e5fe02ee5fcf66bb99fab1d3f3ce592
ItAlexander <sashabronya@gmail.com>
2022-11-01 23:58:10 +0300

    US preset added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
+import dearpygui.dearpygui as dpg
 
 def maybeMakeNumber(s):  
-    # handle None, "", 0
     if not s:
         return s
     try:
@@ -18,14 +18,53 @@ def is_integer_num(n):
         return n.is_integer()
     return False
 
+def calcRange(burn, mAcc, mSpeed, ySpeed,eSpeed,dirAng):
+    ykmps = ySpeed * 1.852 / 3600
+    mAkmps = (mAcc * 1.852) / 3600
+    mVkmps = (mSpeed * 1.852) / 3600
+    ekmps = (eSpeed * 1.852) / 3600
+    accTime = (mVkmps-ykmps) / mAkmps
+    if accTime < burn:
+        conTime = burn - accTime
+    else:
+        conTime = 0
+        accTime = burn
+    mDisAc = ykmps*accTime + (mAkmps * accTime * accTime) / 2
+    mDisCon = mVkmps * conTime
+    mDis = mDisAc + mDisCon
+    eDis = ekmps * burn
+    if dirAng == 180 or dirAng == 0:
+        if dirAng == 180:
+            dis = mDis - eDis
+        if dirAng == 0:
+            dis = mDis + eDis
+    else:
+        mAng = math.degrees(math.asin((eDis * math.sin(math.radians(dirAng)) / mDis)))
+        cAng = 180 - mAng - dirAng
+        dis = (mDis * math.sin(math.radians(cAng))) / math.sin(
+            math.radians(dirAng))
+    return dis
+
 print("Welcome To sashabronya's optimal firing range for missiles calculator. Original idea of realmadridmydestiny1.")
 print("version 1.0 All rights reserved. 2022")
 print("----------------------------------------------------")
 mode = "c"
-mode = input("What mode to use(p-preset,c-custom)")
-if mode == "p":
+mode = input("What mode to use(ru or us-preset,c-custom): ")
+if mode == "ru":
   print("----------------------------------------------------")
-  with open("missiles.txt", "r") as f:
+  with open("missiles_ru.txt", "r") as f:
+    i=0
+    lines = f.readlines()
+    for line in lines:
+        print(line)
+  print("----------------------------------------------------")
+  id=(int)(input("Missile id: "))
+  ar=list(map(str,lines[id].split(" ")))
+  statArray = list(map(int,filter(is_integer_num,list(map(maybeMakeNumber, ar)))))
+  burn, mAcc, mSpeed = statArray[1], statArray[2], statArray[3]
+if mode == "us":
+  print("----------------------------------------------------")
+  with open("missiles_us.txt", "r") as f:
     i=0
     lines = f.readlines()
     for line in lines:
@@ -46,29 +85,4 @@ eSpeed = (int)(input("What's the enemy's speed: "))
 dirAng = (int)(input("What's target ditection relatively to you(angle from 0(towards you) to 180 (from you): "))
 ySpeed = (int)(input("What is your speed?: "))
 print("----------------------------------------------------")
-ykmps = ySpeed * 1.852 / 3600
-mAkmps = (mAcc * 1.852) / 3600
-mVkmps = (mSpeed * 1.852) / 3600
-ekmps = (eSpeed * 1.852) / 3600
-accTime = (mVkmps-ykmps) / mAkmps
-if accTime < burn:
-    conTime = burn - accTime
-else:
-    conTime = 0
-    accTime = burn
-mDisAc = ykmps*accTime + (mAkmps * accTime * accTime) / 2
-mDisCon = mVkmps * conTime
-mDis = mDisAc + mDisCon
-eDis = ekmps * burn
-if dirAng == 180 or dirAng == 0:
-    if dirAng == 180:
-        dis = mDis - eDis
-    if dirAng == 0:
-        dis = mDis + eDis
-else:
-    mAng = math.degrees(
-        math.asin((eDis * math.sin(math.radians(dirAng)) / mDis)))
-    cAng = 180 - mAng - dirAng
-    dis = (mDis * math.sin(math.radians(cAng))) / math.sin(
-        math.radians(dirAng))
-print("Max effective range: "+ str(dis))
+print("Max effective range: "+ str(calcRange(burn, mAcc, mSpeed, ySpeed,eSpeed,dirAng)))
